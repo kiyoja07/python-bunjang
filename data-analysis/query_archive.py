a3390c15ae1ede194450a0b59260105168cc13a3
kiyoja07 <kiyoja07@gmail.com>
2019-06-12 18:08:35 +0900

    2019-06-12

--- data-analysis/query_archive.py
+++ data-analysis/query_archive.py
@@ -5,6 +5,43 @@ Query Archive
 
 
 
+
+"""
+Query & Save Path
+"""
+
+# path to save
+save_path = 'csv/clicked.csv'
+
+
+# macro parameters
+start_time = '2018-01-01 00:00:00'
+end_time = '2019-05-01 00:00:00'
+
+# updated >= %(start_time)s AND updated < %(end_time)s
+
+# query to run
+query = """
+
+-- 일별 카테고리별 상품 클릭 수
+
+SELECT tmp.date_at, tmp.category_id, c.name, tmp.clicked
+FROM (
+    SELECT date_trunc('day', i.updated) AS date_at, p.category_id, COUNT(i.id) AS clicked
+    FROM item_click_log6 i
+    LEFT JOIN product_info_for_stats p
+    ON i.target_id = p.pid
+    WHERE i.updated >= %(start_time)s AND i.updated < %(end_time)s
+    GROUP BY 1, 2
+) tmp
+LEFT JOIN categories c
+ON tmp.category_id = c.category
+
+
+"""
+
+
+
 """
 Query & Save Path
 """
@@ -36,12 +73,13 @@ GROUP BY 1, 2, 3, 4
 
 
 
+
 """
 Query & Save Path
 """
 
 # path to save
-save_path = 'csv/bunp_category_since_1807.csv'
+save_path = 'csv/bunp_category_since_1801.csv'
 
 
 # macro parameters
@@ -54,22 +92,22 @@ save_path = 'csv/bunp_category_since_1807.csv'
 query = """
 
 
-
+-- 카테고리별 번프 완료된 상품 수
 
 -- 전체 구매 데이터
 WITH bunp_all AS (
 SELECT seller_pid, date_trunc('day', updated_at) AS updated_at, seller_pid_price AS bunp_amount
 FROM bunjang_promise
 WHERE status = 4 AND seller_pid_price > 0 AND seller_pid_price < 3000000 AND 
-    updated_at >= '2017-01-01' AND updated_at < '2019-05-01'
+    updated_at >= '2018-01-01' AND updated_at < '2019-05-01'
 ),
--- 구매 상품의 카테고리 id, 18년 7월 1일 이후 등록된 상품만
+-- 구매 상품의 카테고리 id, 18년 1월 1일 이후 등록된 상품만
 bunp_cate AS (
 SELECT category_id AS cate_id, a.updated_at, a.seller_pid, a.bunp_amount
 FROM bunp_all a
 JOIN product_info_for_stats p
 ON a.seller_pid = p.pid
-where create_date >= '2018-07-01 00:00:00'
+where create_date >= '2018-01-01 00:00:00'
 )
 
 
@@ -82,7 +120,6 @@ ON b.cate_id = c.category
 
 
 """
-
 
 
 
