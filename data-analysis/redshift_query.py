573cecb247bdaf164f9af0f09faecfc8f4a5ee58
kiyoja07 <kiyoja07@gmail.com>
2019-07-19 18:03:14 +0900

    2019-07-19

--- data-analysis/redshift_query.py
+++ data-analysis/redshift_query.py
@@ -2,7 +2,6 @@
 
 
 
-
 """
 DB : Redshift, PostgreSQL
 Query & Save Path
@@ -10,25 +9,25 @@ Query & Save Path
 
 
 # path to save
-save_path = 'csv/test.csv'
+save_path = 'csv/user.csv'
 
 # macro parameters
-start_time = '2019-01-01 00:00:00'
-end_time = '2019-04-01 00:00:00'
+start_time = '2017-01-01 00:00:00'  # 이상
+end_time = '2019-07-01 00:00:00'  # 미만
 
-interval_type = 'months'
-# interval_type = None
+# interval_type = 'months'
+interval_type = None
 
 # updated >= %(start_time)s AND updated < %(end_time)s
 
 # query to run
 query = """
 
-select id
-from user_join_log
-where updated >= %(start_time)s AND updated < %(end_time)s
-
+select uid, join_date, age, sex, bizlicense, device
+from user_for_stats
+where join_date >= '2017-01-01 00:00:00' AND join_date < '2019-07-01 00:00:00'
 
 """
 
 # ---------------------------------------------------------------------------------
+
--- data-analysis/redshift_query_archive.py
+++ data-analysis/redshift_query_archive.py
@@ -4,6 +4,69 @@ Query Archive
 """
 
 
+"""
+DB : Redshift, PostgreSQL
+Query & Save Path
+"""
+
+
+# path to save
+save_path = 'csv/user.csv'
+
+# macro parameters
+start_time = '2017-01-01 00:00:00'  # 이상
+end_time = '2019-07-01 00:00:00'  # 미만
+
+# interval_type = 'months'
+interval_type = None
+
+# updated >= %(start_time)s AND updated < %(end_time)s
+
+# query to run
+query = """
+
+select uid, join_date, age, sex, bizlicense, device
+from user_for_stats
+where join_date >= '2017-01-01 00:00:00' AND join_date < '2019-07-01 00:00:00'
+
+"""
+
+# ---------------------------------------------------------------------------------
+
+
+
+"""
+DB : Redshift, PostgreSQL
+Query & Save Path
+"""
+
+
+# path to save
+save_path = 'csv/product.csv'
+
+# macro parameters
+start_time = '2017-01-01 00:00:00'  # 이상
+end_time = '2019-07-01 00:00:00'  # 미만
+
+interval_type = 'months'
+# interval_type = None
+
+# updated >= %(start_time)s AND updated < %(end_time)s
+
+# query to run
+query = """
+
+select pid, uid, create_date, category_id, price
+from product_info_for_stats
+where create_date >= %(start_time)s AND create_date < %(end_time)s
+
+"""
+
+# ---------------------------------------------------------------------------------
+
+
+
+
 
 """
 DB : Redshift, PostgreSQL
