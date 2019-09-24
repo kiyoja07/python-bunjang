85edc362d1a28f9186866fd7f0d351c1f8af032f
kiyoja07 <kiyoja07@gmail.com>
2019-09-24 18:12:30 +0900

    add buntalk channel

--- data-analysis/run-sql/redshift/run_redshift_query.py
+++ data-analysis/run-sql/redshift/run_redshift_query.py
@@ -8,6 +8,47 @@ from datetime import *
 from dateutil.relativedelta import relativedelta
 from save_query_result import save_query_result
 
+
+def run_query_redshift(query, save_path, db_name, start_time=None, end_time=None, interval_type=None):
+
+    if start_time and end_time and interval_type:
+
+        # string -> datetime object
+        start_time = datetime.strptime(start_time, "%Y-%m-%d %H:%M:%S")
+        end_at = datetime.strptime(end_time, "%Y-%m-%d %H:%M:%S")
+
+        query_count = 0
+
+        interval_num = 1
+        kwargs = {interval_type: interval_num}
+
+        while start_time < end_at:
+            end_time = start_time + relativedelta(**kwargs)
+
+            print(query_count, ' : ', start_time, ' ~ ', end_time, ' , run at : ', datetime.now().time())
+
+            query_params = {'start_time': start_time, 'end_time': end_time}
+
+            result = connect_redshift(query, db_name, query_params)  # run query
+
+            save_query_result(save_path, result, query_count)  # save to csv
+
+            # update loop parameters
+            start_time = end_time
+            query_count += 1
+
+    elif (start_time and end_time and interval_type) == 0:
+
+        print('run at :', datetime.now().time())  # print start time
+
+        result = connect_redshift(query, db_name)  # run query
+
+        save_query_result(save_path, result)  # save to csv
+
+    return None
+
+
+
 def run_query_redshift_without_macro(query, save_path, db_name):
     """ 매크로 없이 쿼리 실행 """
 
@@ -23,6 +64,7 @@ def run_query_redshift_without_macro(query, save_path, db_name):
 def run_query_redshift_macro(query, save_path, db_name, start_time, end_time, interval_type):
     """ 기간 별로 매크로를 돌려서 쿼리 실행 """
 
+    # string -> datetime object
     start_time = datetime.strptime(start_time, "%Y-%m-%d %H:%M:%S")
     end_at = datetime.strptime(end_time, "%Y-%m-%d %H:%M:%S")
 
