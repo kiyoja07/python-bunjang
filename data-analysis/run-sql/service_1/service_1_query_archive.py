a37a0553e5ab43a7f625593c36cff06a7131ee78
kiyoja07 <kiyoja07@gmail.com>
2019-11-15 14:25:33 +0900

    2019-11-15

--- data-analysis/run-sql/service_1/service_1_query_archive.py
+++ data-analysis/run-sql/service_1/service_1_query_archive.py
@@ -12,14 +12,14 @@ Query & Save Path
 
 
 # path to save
-save_path = 'csv/reward_test_bunp.csv'
+save_path = 'csv/reward_test_product_191107_191113.csv'
 
 # macro parameters
-start_time = '2019-10-22 00:00:00.0'  # 이상
-end_time = '2019-11-01 00:00:00.0'  # 미만
+# start_time = '2019-10-22 00:00:00.0'  # 이상
+# end_time = '2019-11-01 00:00:00.0'  # 미만
 
 # interval_type = None
-interval_type = 'days'
+# interval_type = 'days'
 # interval_type = 'months'
 
 
@@ -28,9 +28,16 @@ interval_type = 'days'
 # query to run
 query = """
 
-select channel_id, seller_uid, buyer_uid, seller_pid, created_at
-from bunjang_promise
-where created_at >= %(start_time)s AND created_at < %(end_time)s
+select u.uid, p.create_date, p.id as pid
+from (
+    select distinct u.id as uid
+    from user u
+    where u.status = 0 # 탈퇴 유저 제외
+        and u.join_date between '2019-10-21 00:00:00' and '2019-10-23 23:59:59' # 이벤트 대상 가입일
+) u
+join product_info p
+on u.uid = p.uid
+where p.create_date between '2019-11-07 00:00:00' and '2019-11-13 23:59:59' # 상품 등록 기간
 
 
 """
