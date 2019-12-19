77bb9e9119e529d235d597a008b02c2878919e8f
kiyoja07 <kiyoja07@gmail.com>
2019-12-19 17:32:36 +0900

    complete analyze 2th reward event

--- data-analysis/run-sql/service_1/service_1_query.py
+++ data-analysis/run-sql/service_1/service_1_query.py
@@ -10,7 +10,7 @@ Query & Save Path
 
 
 # path to save
-save_path = 'csv/reward_register_2.0_all_user.csv'
+save_path = 'csv/reward_register_2.0_product.csv'
 
 # macro parameters
 # start_time = '2019-10-22 00:00:00.0'  # 이상
@@ -26,9 +26,16 @@ interval_type = None
 # query to run
 query = """
 
-select date_format(u.join_date, '%Y-%m-%d') as join_date, u.id as uid
-from user u
-where date_format(u.join_date, '%Y-%m-%d') in ('2019-12-04', '2019-12-05', '2019-12-08')
+select u.uid, p.create_date, p.id as pid
+from (
+    select distinct u.id as uid
+    from user u
+    where u.status = 0 # 탈퇴 유저 제외
+        and date_format(u.join_date, '%Y-%m-%d') in ('2019-12-04', '2019-12-05', '2019-12-08') # 이벤트 대상 가입일
+) u
+join product_info p
+on u.uid = p.uid
+where p.create_date between '2019-12-05 00:00:00' and '2019-12-16 23:59:59' # 상품 등록 기간
 
 
 """
